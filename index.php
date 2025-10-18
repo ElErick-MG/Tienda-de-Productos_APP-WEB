71f2f36bd45db8637cba66841a16c826804e81ef
A-PerfectStranger <eduardoarcos6to@gmail.com>
2025-10-18 15:33:27 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -14,11 +14,13 @@
             Usuario:<br>
             <input type="text" name="nombre" required value="<?php echo $stored_nombre; ?>"/><br>
             Clave:<br>
-            <input type="text" name="clave" id="clave" required value="<?php echo $stored_clave; ?>"/><br>
+            <input type="text" id="clave" required/><br>
+            <input type="hidden" name="clave" id="clave_real"/>
             <input type="checkbox" name="recordar" <?php echo $remember_checked; ?>/>Recordarme<br>
             <script>
                 const form = document.getElementById("form");
                 const input = document.getElementById("clave");
+                const inputReal = document.getElementById("clave_real");
                 let realPassword = "<?php echo $stored_clave; ?>";  
                 
                 if (realPassword.length > 0) {
@@ -32,10 +34,12 @@
                     } else {
                         realPassword = realPassword.slice(0, current.length);
                     }
+                    inputReal.value = realPassword;
                     input.value = "*".repeat(realPassword.length);
                 });
+
                 form.addEventListener("submit", (e) => {
-                    input.value = realPassword;
+                    input.disabled = true;
                 });
             </script>
             <input type="submit" name="btnEnviar" value="Enviar"/>
