0866cc060ae94bae825b3c57c005c5d557175d91
GeovannyPuchaicela <cg.puchaicela@gmail.com>
2025-10-17 22:18:52 -0500

    Conexion BD

--- conexion/producto.php
+++ conexion/producto.php
@@ -1,30 +1,42 @@
 <?php
-include_once 'producto.php';
 
-$productos = array();
+class Producto {
+    private $id;
+    private $nombre;
+    private $descripcion;
+    private $precio;
 
-$producto1 = new Producto();
-$producto1->setId(1);
-$producto1->setNombre("Producto A");
-$producto1->setDescripcion("Descripción del Producto A");
-$producto1->setPrecio(15.50);
+    function __construct() {
+    }
 
-array_push($productos, $producto1);
-$producto2 = new Producto();
-$producto2->setId(2);
-$producto2->setNombre("Producto B");
-$producto2->setDescripcion("Descripción del Producto B");
-$producto2->setPrecio(25.00);
+    public function getId() {
+        return $this->id;
+    }
 
-array_push($productos, $producto2);
+    public function getNombre() {
+        return $this->nombre;
+    }
 
-$producto3 = new Producto();
-$producto3->setId(3);
-$producto3->setNombre("Producto C");
-$producto3->setDescripcion("Descripción del Producto C");
-$producto3->setPrecio(30.75);
-array_push($productos, $producto3);
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
 
-foreach ($productos as $prod) {
-    echo "<h3>$prod</h3>";
+    public function getPrecio() {
+        return $this->precio;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+    public function __toString() {
+        return "ID: " . $this->id . ", Nombre: " . $this->nombre . ", Descripción: " . $this->descripcion . ", Precio: $" . $this->precio;
+    }
 }
